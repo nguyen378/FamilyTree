b067681f98cae520c1dbdb90528cb832c0f19a9d
PhatNguyen3174 <phatnguyen9712@gmail.com>
2023-10-22 23:17:28 +0700

    comit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,9 @@ module donutnv.familytree {
     opens donutnv.familytree to javafx.fxml;
     exports donutnv.familytree;
     requires org.neo4j.driver;
+    requires java.desktop;
+    requires java.logging;
+    requires org.jgrapht.core;
+    requires org.jgrapht.ext;
+    requires org.jgrapht.demo;
 }
