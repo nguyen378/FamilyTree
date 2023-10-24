21dda89128f0ae6902ea4b2e76cf0e1e07003e06
Thương Nguyễn <thuongnguyen1613802@gmail.com>
2023-10-24 23:42:46 +0700

    new

--- src/main/java/donutnv/familytree/App.java
+++ src/main/java/donutnv/familytree/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 800, 450);
+        scene = new Scene(loadFXML("livingat"), 800, 450);
         stage.setScene(scene);
         stage.show();
         stage.setTitle("Login Form");
@@ -30,6 +30,7 @@ public class App extends Application {
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        
         return fxmlLoader.load();
     }
 
--- src/main/java/donutnv/familytree/Controller/HomeController.java
+++ src/main/java/donutnv/familytree/Controller/HomeController.java
@@ -4,9 +4,18 @@
  */
 package donutnv.familytree.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -18,6 +27,27 @@ public class HomeController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    private Button buttonlv; 
+    @FXML
+    private void buttonliving(ActionEvent event)
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/donutnv/familytree/livingat.fxml"));
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+           Node sourceNode = (Node) event.getSource();
+            Stage stage = (Stage) sourceNode.getScene().getWindow();
+            stage.close();
+            Stage newStage = new Stage();
+            newStage.setTitle("Living At");
+            newStage.setScene(scene);
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
