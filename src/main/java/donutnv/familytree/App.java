d5447ccbf013f0cf40422bcf173b0ba80dbb28e2
Thương Nguyễn <thuongnguyen1613802@gmail.com>
2023-10-25 08:17:05 +0700

    new_1

--- src/main/java/donutnv/familytree/App.java
+++ src/main/java/donutnv/familytree/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("livingat"), 800, 450);
+        scene = new Scene(loadFXML("login"), 800, 450);
         stage.setScene(scene);
         stage.show();
         stage.setTitle("Login Form");
